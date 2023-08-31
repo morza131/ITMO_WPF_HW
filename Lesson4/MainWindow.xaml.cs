b0fc9a536431c4eb17fe34a97d609a95f523a1b9
unknown <morza131@gmail.com>
2023-08-31 20:46:15 +0300

    выполнено задание урока 4

--- Lesson4/MainWindow.xaml.cs
+++ Lesson4/MainWindow.xaml.cs
@@ -29,13 +29,13 @@ namespace Lesson4
         private void dollarCalculate_Click(object sender, RoutedEventArgs e)
         {
             dollarResult.Text = (Convert.ToDouble(dollarExRate.Text)*Convert.ToDouble(dollarValue.Text)).ToString();
-            WebClient client = new WebClient();
+            /*WebClient client = new WebClient();
             var xml = client.DownloadString("https://www.cbr-xml-daily.ru/daily.xml");
             XDocument xdoc = XDocument.Parse(xml);
             var el = xdoc.Element("ValCurs").Elements("Valute");
             string dollar = el.Where(x => x.Attribute("ID").Value == "R01235").Select(x => x.Element("Value").Value).FirstOrDefault();
             string eur = el.Where(x => x.Attribute("ID").Value == "R01239").Select(x => x.Element("Value").Value).FirstOrDefault();
-            MessageBox.Show($"Евро: {eur} Доллар: {dollar}");
+            MessageBox.Show($"Евро: {eur} Доллар: {dollar}");*/
         }
 
         private void euroCalculate_Click(object sender, RoutedEventArgs e)
@@ -53,15 +53,61 @@ namespace Lesson4
             dramaResult.Text = (Convert.ToDouble(dramaExRate.Text) * Convert.ToDouble(dramaValue.Text)).ToString();
         }
 
-        private void meters_TextChanged(object sender, TextChangedEventArgs e)
+     
+
+        private void meters_KeyDown(object sender, KeyEventArgs e)
         {
-            if (meters.Text != "")
+            if (e.Key == Key.Enter)
             {
-                double _meters = Convert.ToDouble(meters.Text);
-                foots.Text = (_meters * 0.3048).ToString();
-                arshins.Text = (_meters * 0.7112).ToString();
-                miles.Text = (_meters * 1609.34).ToString();
+                if (meters.Text != "")
+                {
+                    double _meters = Convert.ToDouble(meters.Text);
+                    foots.Text = (_meters * 0.3048).ToString();
+                    arshins.Text = (_meters * 0.7112).ToString();
+                    miles.Text = (_meters / 1609.34).ToString();
+                }
             }
+        }
+
+        private void foots_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (foots.Text != "")
+                {
+                    double _foots = Convert.ToDouble(foots.Text);
+                    meters.Text = (_foots * 3.28084).ToString();
+                    arshins.Text = (_foots * 2.33333).ToString();
+                    miles.Text = (_foots / 5280).ToString();
+                }
+            }
+        }
+
+        private void arshins_KeyDown(object sender, KeyEventArgs e)
+        {
+                if (e.Key == Key.Enter)
+                {
+                    if (arshins.Text != "")
+                    {
+                        double _arshins = Convert.ToDouble(arshins.Text);
+                        foots.Text = (_arshins * 0.42857).ToString();
+                        meters.Text = (_arshins * 1.406).ToString();
+                        miles.Text = (_arshins / 2262.857).ToString();
+                    }
+                }
+        }
+        private void miles_KeyDown(object sender, KeyEventArgs e)
+        {
+                if (e.Key == Key.Enter)
+                {
+                    if (miles.Text != "")
+                    {
+                        double _miles = Convert.ToDouble(miles.Text);
+                        foots.Text = (_miles / 0.0001893939393939).ToString();
+                        arshins.Text = (_miles / 0.0004419191919192).ToString();
+                        meters.Text = (_miles / 0.0006213711922372).ToString();
+                    }
+                }
         }
     }
 }
