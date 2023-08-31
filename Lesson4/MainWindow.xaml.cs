f478583c2559f527c9944ad4a09dff5ce4ad3a4d
morza131 <morza131@gmail.com>
2023-08-31 17:25:28 +0300

    added files for lesson4

--- Lesson4/MainWindow.xaml.cs
+++ Lesson4/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace Lesson4
 {
@@ -23,6 +25,43 @@ namespace Lesson4
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void dollarCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            dollarResult.Text = (Convert.ToDouble(dollarExRate.Text)*Convert.ToDouble(dollarValue.Text)).ToString();
+            WebClient client = new WebClient();
+            var xml = client.DownloadString("https://www.cbr-xml-daily.ru/daily.xml");
+            XDocument xdoc = XDocument.Parse(xml);
+            var el = xdoc.Element("ValCurs").Elements("Valute");
+            string dollar = el.Where(x => x.Attribute("ID").Value == "R01235").Select(x => x.Element("Value").Value).FirstOrDefault();
+            string eur = el.Where(x => x.Attribute("ID").Value == "R01239").Select(x => x.Element("Value").Value).FirstOrDefault();
+            MessageBox.Show($"Евро: {eur} Доллар: {dollar}");
+        }
+
+        private void euroCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            euroResult.Text = (Convert.ToDouble(euroExRate.Text) * Convert.ToDouble(euroValue.Text)).ToString();
+        }
+
+        private void hryvniaCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            hryvniaResult.Text = (Convert.ToDouble(hryvniaExRate.Text) * Convert.ToDouble(hryvniaValue.Text)).ToString();
+        }
+
+        private void dramaCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            dramaResult.Text = (Convert.ToDouble(dramaExRate.Text) * Convert.ToDouble(dramaValue.Text)).ToString();
+        }
+
+        private void meters_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (meters.Text != "")
+            {
+                double _meters = Convert.ToDouble(meters.Text);
+                foots.Text = (_meters * 0.3048).ToString();
+                arshins.Text = (_meters * 0.7112).ToString();
+                miles.Text = (_meters * 1609.34).ToString();
+            }
         }
     }
 }
