7a56b2c6ce18b127d4b0d988f2cffb1910afb2c9
morza131 <morza131@gmail.com>
2023-10-31 11:26:01 +0300

    Выполнено задание урока 17

---
+++ Lesson17/RGBColor.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lesson17
+{
+    /// <summary>
+    /// Логика взаимодействия для RGBColor.xaml
+    /// </summary>
+    public partial class RGBColor : UserControl
+    {
+        public static DependencyProperty ColorProperty;
+        public static DependencyProperty RedProperty;
+        public static DependencyProperty GreenProperty;
+        public static DependencyProperty BlueProperty;
+        public RGBColor()
+        {
+            InitializeComponent();
+        }
+        static RGBColor()
+        {
+            // Регистрация свойств зависимости
+            ColorProperty = DependencyProperty.Register("Color", typeof(Color), typeof(RGBColor),
+                new FrameworkPropertyMetadata(Colors.Black, new PropertyChangedCallback(OnColorChanged)));
+            RedProperty = DependencyProperty.Register("Red", typeof(byte), typeof(RGBColor),
+                new FrameworkPropertyMetadata(new PropertyChangedCallback(OnColorRGBChanged)));
+            GreenProperty = DependencyProperty.Register("Green", typeof(byte), typeof(RGBColor),
+                new FrameworkPropertyMetadata(new PropertyChangedCallback(OnColorRGBChanged)));
+            BlueProperty = DependencyProperty.Register("Blue", typeof(byte), typeof(RGBColor),
+                 new FrameworkPropertyMetadata(new PropertyChangedCallback(OnColorRGBChanged)));
+            ColorChangedEvent = EventManager.RegisterRoutedEvent("ColorChanged", RoutingStrategy.Bubble,
+    typeof(RoutedPropertyChangedEventHandler<Color>), typeof(RGBColor));
+        }
+        public Color Color
+        {
+            get { return (Color)GetValue(ColorProperty); }
+            set { SetValue(ColorProperty, value); }
+        }
+        public byte Red
+        {
+            get { return (byte)GetValue(RedProperty); }
+            set { SetValue(RedProperty, value); }
+        }
+        public byte Green
+        {
+            get { return (byte)GetValue(GreenProperty); }
+            set { SetValue(GreenProperty, value); }
+        }
+        public byte Blue
+        {
+            get { return (byte)GetValue(BlueProperty); }
+            set { SetValue(BlueProperty, value); }
+        }
+        private static void OnColorRGBChanged(DependencyObject sender,
+            DependencyPropertyChangedEventArgs e)
+        {
+            RGBColor colorPicker = (RGBColor)sender;
+            Color color = colorPicker.Color;
+            if (e.Property == RedProperty)
+                color.R = (byte)e.NewValue;
+            else if (e.Property == GreenProperty)
+                color.G = (byte)e.NewValue;
+            else if (e.Property == BlueProperty)
+                color.B = (byte)e.NewValue;
+
+            colorPicker.Color = color;
+        }
+        private static void OnColorChanged(DependencyObject sender,
+      DependencyPropertyChangedEventArgs e)
+        {
+            Color newColor = (Color)e.NewValue;
+            RGBColor colorpicker = (RGBColor)sender;
+            colorpicker.Red = newColor.R;
+            colorpicker.Green = newColor.G;
+            colorpicker.Blue = newColor.B;
+        }
+        public static readonly RoutedEvent ColorChangedEvent;
+        public event RoutedPropertyChangedEventHandler<Color> ColorChanged
+        {
+            add { AddHandler(ColorChangedEvent, value); }
+            remove { RemoveHandler(ColorChangedEvent, value); }
+        }
+
+    }
+}
