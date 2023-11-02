22d9ccced964ce7385aa2bdc09a58161787cd6ce
morza131 <morza131@gmail.com>
2023-11-03 00:47:42 +0300

    Выполнено задание урока 19

---
+++ Lesson19/Model/MyMath.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson19.Model
+{
+    static class MyMath
+    {
+        public static double FindCircumference(int a) => a * 2 * 3.14;
+    }
+}
---
+++ Lesson19/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Lesson19.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Lesson19.ViewModel
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;  
+        void OnPropertyChanged([CallerMemberName]string propertyName=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private int radius;
+        public int Radius
+        {
+            get => radius;
+            set
+            {                
+                    radius = value;
+                OnPropertyChanged();
+            }
+        }
+        private double circumference;
+        public double Сircumference
+        {
+            get => circumference;
+            set
+            {
+                circumference = value;
+                OnPropertyChanged();
+            }
+        }
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p)
+        {
+            Сircumference = MyMath.FindCircumference(Radius);
+        }
+        private bool CanAddCommandExecuted(object p)
+        {
+           if (Radius != 0)
+                return true;
+           else
+            return false;
+        }
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+    }
+}
